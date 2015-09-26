deea0f6e41a62f9bce273f700c15020bb8b426a5
sophie.hantzperg <sophie.hantzperg@uha.fr>
2015-09-26 10:02:40 +0200

    Création du projet

---
+++ location-vacances/src/main/java/demo/LocationVacancesApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVacancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationVacancesApplication.class, args);
+    }
+}
