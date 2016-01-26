8b89ce24e071653a88d7cc1b3a103c73f64cc325
e1302589 <e1302589@FST-I39-PC02.uha.fr>
2016-01-26 17:42:50 +0100

    Inscription + gestion users + gestion logements (opérationnels) + connexion (non opérationnel)

--- location-vacances/src/main/java/demo/controller/ConnexionController.java
+++ location-vacances/src/main/java/demo/controller/ConnexionController.java
@@ -1,11 +1,17 @@
 package demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import demo.model.Login;
+import demo.model.Utilisateur;
 import demo.repository.LoginRepository;
 
 @Controller
@@ -13,10 +19,29 @@ public class ConnexionController {
 	@Autowired
 	private LoginRepository loginRespository;
 	
+	
 	@RequestMapping("/connexion")
-	public String requestConnexion(@PathVariable("id") Integer userId, RedirectAttributes redirectAttribute)
+	public String requestInscription(Model model)
 	{
-		redirectAttribute.addFlashAttribute("id", userId);
-		return "redirect:/suscribersHome";
+		model.addAttribute("user", new Utilisateur());		
+		return "connexion";
+	}
+	
+	@RequestMapping(value="/connexion", method=RequestMethod.POST)
+	public String requestConnexion(Login login, RedirectAttributes redirectAttribute)
+	{
+		List<Login> loginList = (List<Login>)loginRespository.findAll();
+		Login result = loginList.stream()
+				.filter(user -> user.getLogin().equals(login.getLogin()) && user.getPassword().equals(login.getPassword()))
+				.findFirst()
+				.get();
+		
+		if(result != null) {
+			return "redirect:/suscribersHome";
+		}
+		else {
+			return "connexion";
+		}
+		
 	}
 }
